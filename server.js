b941154cd22a631029292eb243078d5e52126c18
LourensCDA <lourens@cdasolutions.co.za>
2021-09-12 07:02:40 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+// load config file
+dotenv.config({ path: './config/config.env' });
+
+const app = express();
+
+var corsOptions = {
+  origin: process.env.CORSURL + ':' + process.env.PORT,
+};
+
+app.use(cors(corsOptions));
+
+// simple route
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to bezkoder application.' });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 5080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
